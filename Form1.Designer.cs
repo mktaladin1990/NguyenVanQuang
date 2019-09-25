532e49089dc810c0bafa036b74354d207ac29d1c
mktaladin1990 <vanquanid11@gmail.com>
2019-09-25 23:42:21 +0700

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace SlideShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.next = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "IMG_0062.JPG");
+            this.imageList1.Images.SetKeyName(1, "IMG_0096.jpeg");
+            this.imageList1.Images.SetKeyName(2, "IMG_0147.JPG");
+            this.imageList1.Images.SetKeyName(3, "IMG_0696.JPG");
+            this.imageList1.Images.SetKeyName(4, "IMG_2029.JPG");
+            // 
+            // next
+            // 
+            this.next.Image = global::SlideShow.Properties.Resources.right_arrow;
+            this.next.Location = new System.Drawing.Point(767, 556);
+            this.next.Margin = new System.Windows.Forms.Padding(4);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(49, 32);
+            this.next.TabIndex = 13;
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.Next_Click_1);
+            // 
+            // prev
+            // 
+            this.prev.Image = global::SlideShow.Properties.Resources.left_arrow;
+            this.prev.Location = new System.Drawing.Point(228, 556);
+            this.prev.Margin = new System.Windows.Forms.Padding(4);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(49, 32);
+            this.prev.TabIndex = 12;
+            this.prev.UseVisualStyleBackColor = true;
+            this.prev.Click += new System.EventHandler(this.Prev_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.ImageIndex = 0;
+            this.button5.ImageList = this.imageList1;
+            this.button5.Location = new System.Drawing.Point(641, 554);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 39);
+            this.button5.TabIndex = 9;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.ImageIndex = 1;
+            this.button4.ImageList = this.imageList1;
+            this.button4.Location = new System.Drawing.Point(563, 554);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 39);
+            this.button4.TabIndex = 8;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ImageIndex = 2;
+            this.button3.ImageList = this.imageList1;
+            this.button3.Location = new System.Drawing.Point(484, 554);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 39);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ImageIndex = 4;
+            this.button2.ImageList = this.imageList1;
+            this.button2.Location = new System.Drawing.Point(405, 554);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 39);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.ImageIndex = 3;
+            this.button1.ImageList = this.imageList1;
+            this.button1.Location = new System.Drawing.Point(327, 554);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 39);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::SlideShow.Properties.Resources.IMG_0062;
+            this.pictureBox5.Location = new System.Drawing.Point(136, 15);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(817, 503);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SlideShow.Properties.Resources.IMG_0096;
+            this.pictureBox4.Location = new System.Drawing.Point(136, 15);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(817, 503);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SlideShow.Properties.Resources.IMG_0147;
+            this.pictureBox3.Location = new System.Drawing.Point(136, 15);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(817, 503);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SlideShow.Properties.Resources.IMG_2029;
+            this.pictureBox2.Location = new System.Drawing.Point(136, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(817, 503);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SlideShow.Properties.Resources.IMG_0696;
+            this.pictureBox1.Location = new System.Drawing.Point(136, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(817, 503);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 608);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button prev;
+        private System.Windows.Forms.Button next;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SlideShow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        #region Click button
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible == false)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Visible == false)
+            {
+                pictureBox2.Visible = true;
+                pictureBox1.Visible = false;
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3.Visible == false)
+            {
+                pictureBox3.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox4.Visible == false)
+            {
+                pictureBox4.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+                pictureBox1.Visible = false;
+                pictureBox5.Visible = false;
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox5.Visible == false)
+            {
+                pictureBox5.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox1.Visible = false;
+                timer1.Stop();
+                timer1.Start();
+            }
+
+        }
+        #endregion
+        #region auto next
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+            else if (pictureBox2.Visible == true)
+            {
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = true;
+            }
+            else if (pictureBox3.Visible == true)
+            {
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = true;
+            }
+            else if (pictureBox4.Visible == true)
+            {
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = true;
+            }
+            else if (pictureBox5.Visible == true)
+            {
+                pictureBox5.Visible = false;
+                pictureBox1.Visible = true;
+            }
+        }
+
+
+        #endregion
+
+        private void Prev_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox5.Visible = true;
+            }
+            else if (pictureBox2.Visible == true)
+            {
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = true;
+            }
+            else if (pictureBox3.Visible == true)
+            {
+                pictureBox3.Visible = false;
+                pictureBox2.Visible = true;
+            }
+            else if (pictureBox4.Visible == true)
+            {
+                pictureBox4.Visible = false;
+                pictureBox3.Visible = true;
+            }
+            else if (pictureBox5.Visible == true)
+            {
+                pictureBox5.Visible = false;
+                pictureBox4.Visible = true;
+            }
+            timer1.Stop();
+            timer1.Start();
+        }
+        private void Next_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+            else if (pictureBox2.Visible == true)
+            {
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = true;
+            }
+            else if (pictureBox3.Visible == true)
+            {
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = true;
+            }
+            else if (pictureBox4.Visible == true)
+            {
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = true;
+            }
+            else if (pictureBox5.Visible == true)
+            {
+                pictureBox5.Visible = false;
+                pictureBox1.Visible = true;
+            }
+            timer1.Stop();
+            timer1.Start();
+        }
+    }
+}
